c7d9b006317250d6ca4dd10b2ff2082a1e10f068
Haydn Ewers <27211+haydn@users.noreply.github.com>
2018-11-12 21:13:15 +1100

    Fix index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const FileBlob = require('@now/build-utils/file-blob');
 const build = require('./build');
 
 exports.build = async ({ files, config }) => {
-  const data = await Object.keys(files)
-    .filter(key => key.startsWith(config.data))
-    .map(key => files[key].toStream())
-    .reduce(async (promise, stream) => {
-      await promise;
-      return FileBlob.fromStream({
-        stream,
-      }).toString();
-    }, Promise.resolve());
-
+  const data = await Promise.all(
+    Object.keys(files)
+      .filter(key => key.startsWith(config.data))
+      .map(key => files[key].toStream())
+      .map(stream =>
+        FileBlob.fromStream({
+          stream,
+        }),
+      ),
+  );
   return build(data);
 };
